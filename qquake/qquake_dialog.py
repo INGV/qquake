5f031b4f7b3c8f12e6aff68dcb82119b3f7a0398
Nyall Dawson <nyall.dawson@gmail.com>
2020-01-30 10:05:32 +1000

    Remember dialog settings between runs

--- qquake/qquake_dialog.py
+++ qquake/qquake_dialog.py
@@ -33,8 +33,10 @@ from qgis.PyQt.QtWidgets import (
 from qgis.core import (
     QgsProject,
     QgsRectangle,
-    QgsCoordinateReferenceSystem
+    QgsCoordinateReferenceSystem,
+    QgsSettings
 )
+from qgis.gui import QgsGui
 
 from qquake.qquake_defs import (
     fdsn_events_capabilities,
@@ -92,7 +94,57 @@ class QQuakeDialog(QDialog, FORM_CLASS):
 
         self.fetcher = None
 
+        QgsGui.enableAutoGeometryRestore(self)
+
+        self._restore_settings()
         self._refresh_url()
+
+    def _save_settings(self):
+        s = QgsSettings()
+        s.setValue('/plugins/qquake/last_event_service', self.fdsn_event_ws_combobox.currentText())
+        s.setValue('/plugins/qquake/last_event_start_date', self.fdsn_event_start_date.dateTime())
+        s.setValue('/plugins/qquake/last_event_end_date', self.fdsn_event_end_date.dateTime())
+        s.setValue('/plugins/qquake/last_event_min_magnitude',self.fdsn_event_min_magnitude.value())
+        s.setValue('/plugins/qquake/last_event_max_magnitude', self.fdsn_event_max_magnitude.value())
+        s.setValue('/plugins/qquake/last_event_extent', '{}|{}|{}|{}'.format(self.mExtentGroupBox.outputExtent().xMinimum(),
+                                                                             self.mExtentGroupBox.outputExtent().yMinimum(),
+                                                                             self.mExtentGroupBox.outputExtent().xMaximum(),
+                                                                             self.mExtentGroupBox.outputExtent().yMaximum()))
+        s.setValue('/plugins/qquake/last_event_extent_enabled', self.mExtentGroupBox.isChecked())
+
+    def _restore_settings(self):
+        s = QgsSettings()
+        last_service = s.value('/plugins/qquake/last_event_service')
+        if last_service is not None:
+            self.fdsn_event_ws_combobox.setCurrentIndex(self.fdsn_event_ws_combobox.findText(last_service))
+        last_event_start_date = s.value('/plugins/qquake/last_event_start_date')
+        if last_event_start_date is not None:
+            self.fdsn_event_start_date.setDateTime(last_event_start_date)
+        last_event_end_date = s.value('/plugins/qquake/last_event_end_date')
+        if last_event_end_date is not None:
+            self.fdsn_event_end_date.setDateTime(last_event_end_date)
+        last_event_min_magnitude = s.value('/plugins/qquake/last_event_min_magnitude')
+        if last_event_min_magnitude is not None:
+            self.fdsn_event_min_magnitude.setValue(last_event_min_magnitude)
+        last_event_max_magnitude = s.value('/plugins/qquake/last_event_max_magnitude')
+        if last_event_max_magnitude is not None:
+            self.fdsn_event_max_magnitude.setValue(last_event_max_magnitude)
+        last_event_extent = s.value('/plugins/qquake/last_event_extent')
+        if last_event_extent:
+            parts = last_event_extent.split('|')
+            if len(parts) == 4:
+                r = QgsRectangle()
+                try:
+                    r.setXMinimum(float(parts[0]))
+                    r.setYMinimum(float(parts[1]))
+                    r.setXMaximum(float(parts[2]))
+                    r.setYMaximum(float(parts[3]))
+                    self.mExtentGroupBox.setOriginalExtent(r, QgsCoordinateReferenceSystem('EPSG:4326'))
+                except:
+                    pass
+        last_event_extent_enabled = s.value('/plugins/qquake/last_event_extent_enabled')
+        if last_event_extent_enabled is not None:
+            self.mExtentGroupBox.setChecked(bool(last_event_extent_enabled))
 
     def _refresh_date(self):
         """
@@ -151,6 +203,8 @@ class QQuakeDialog(QDialog, FORM_CLASS):
         if self.fetcher:
             # TODO - cancel current request
             return
+
+        self._save_settings()
 
         self.fetcher = self.get_fetcher()
         self.fetcher.progress.connect(self.progressBar.setValue)
