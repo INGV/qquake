ed5a380c894ed8416b2d8b92463747a406e8c4f4
Nyall Dawson <nyall.dawson@gmail.com>
2021-11-30 11:28:01 +1000

    Fix path

--- qquake/test_suite.py
+++ qquake/test_suite.py
@@ -43,7 +43,7 @@ def _run_tests(test_suite, package_name, with_coverage=False):
     print('########')
     if with_coverage:
         cov = coverage.Coverage(
-            source=['/vector_correction'],
+            source=['/qquake'],
             omit=['*/test/*'],
         )
         cov.start()
