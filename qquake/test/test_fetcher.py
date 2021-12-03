0ed5da2c209967708cf4f396eefd6b2d5468ea31
Nyall Dawson <nyall.dawson@gmail.com>
2021-12-03 12:30:17 +1000

    Respect the queryincludeall*_multiple flags when generating urls

--- qquake/test/test_fetcher.py
+++ qquake/test/test_fetcher.py
@@ -12,7 +12,7 @@ import unittest
 from qgis.PyQt.QtCore import QDateTime
 
 from qquake.fetcher import Fetcher
-from qquake.services import ServiceManager
+from qquake.services import ServiceManager, SERVICE_MANAGER
 
 
 class TestFetcher(unittest.TestCase):
@@ -37,6 +37,61 @@ class TestFetcher(unittest.TestCase):
         fetcher.updated_after = QDateTime(2021, 5, 6, 13, 14, 15)
         self.assertEqual(fetcher.generate_url(),
                          'https://www.seismicportal.eu/fdsnws/event/1/query?updatedafter=2021-05-06T13:14:15&limit=1000&format=xml')
+
+    def test_url_no_fetch_multiple(self):
+        """
+        Test generation of urls when include all * multiple is false
+        """
+        fetcher = Fetcher(ServiceManager.FDSNEVENT,
+                          'INGV ISIDe')
+        self.assertTrue(fetcher.preferred_magnitudes_only)
+        self.assertTrue(fetcher.preferred_origins_only)
+        self.assertEqual(fetcher.generate_url(),
+                         'http://webservices.ingv.it/fdsnws/event/1/query?limit=15000&format=xml')
+
+        # if event ids are specified then we can safely get all origins/magnitudes
+        fetcher = Fetcher(ServiceManager.FDSNEVENT,
+                          'INGV ISIDe', event_ids=[1, 2])
+        fetcher.preferred_origins_only = False
+        self.assertEqual(fetcher.generate_url(),
+                         'http://webservices.ingv.it/fdsnws/event/1/query?eventid=1&includeallorigins=true&format=xml')
+
+        fetcher = Fetcher(ServiceManager.FDSNEVENT,
+                          'INGV ISIDe', event_ids=[1, 2])
+        fetcher.preferred_magnitudes_only = False
+        self.assertEqual(fetcher.generate_url(),
+                         'http://webservices.ingv.it/fdsnws/event/1/query?eventid=1&includeallmagnitudes=true&format=xml')
+
+        # without event ids we can't get all origins/magnitudes
+        fetcher = Fetcher(ServiceManager.FDSNEVENT,
+                          'INGV ISIDe')
+        fetcher.preferred_origins_only = False
+        self.assertEqual(fetcher.generate_url(),
+                         'http://webservices.ingv.it/fdsnws/event/1/query?limit=15000&format=xml')
+
+        fetcher = Fetcher(ServiceManager.FDSNEVENT,
+                          'INGV ISIDe')
+        fetcher.preferred_magnitudes_only = False
+        self.assertEqual(fetcher.generate_url(),
+                         'http://webservices.ingv.it/fdsnws/event/1/query?limit=15000&format=xml')
+
+        # a service which doesn't support all magnitudes/origins should never include the parameter
+        SERVICE_MANAGER.services[ServiceManager.FDSNEVENT]['INGV ISIDe']['settings']['queryincludeallorigins'] = False
+        fetcher = Fetcher(ServiceManager.FDSNEVENT,
+                          'INGV ISIDe', event_ids=[1, 2])
+        self.assertTrue(fetcher.preferred_origins_only)
+        self.assertEqual(fetcher.generate_url(),
+                         'http://webservices.ingv.it/fdsnws/event/1/query?eventid=1&format=xml')
+
+        SERVICE_MANAGER.services[ServiceManager.FDSNEVENT]['INGV ISIDe']['settings']['queryincludeallorigins'] = True
+        SERVICE_MANAGER.services[ServiceManager.FDSNEVENT]['INGV ISIDe']['settings'][
+            'queryincludeallmagnitudes'] = False
+
+        fetcher = Fetcher(ServiceManager.FDSNEVENT,
+                          'INGV ISIDe', event_ids=[1, 2])
+        self.assertTrue(fetcher.preferred_magnitudes_only)
+        self.assertEqual(fetcher.generate_url(),
+                         'http://webservices.ingv.it/fdsnws/event/1/query?eventid=1&format=xml')
 
 
 if __name__ == '__main__':
