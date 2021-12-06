c4344da1956a12305ab365d13dd14283753b3b28
Nyall Dawson <nyall.dawson@gmail.com>
2021-12-07 09:38:42 +1000

    Standardise some file headers

--- qquake/gui/gui_utils.py
+++ qquake/gui/gui_utils.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
-"""QQuake- GUI Utilities
-
-.. note:: This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+"""
+Utilities for GUI plugin components
 """
 
+# .. note:: This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 __author__ = 'Original authors: Mario Locati, Roberto Vallone, Matteo Ghetta, Nyall Dawson'
-__date__ = '20/04/2018'
+__date__ = '29/01/2020'
 __copyright__ = 'Istituto Nazionale di Geofisica e Vulcanologia (INGV)'
 # This will get replaced with a git SHA1 when you do a git archive
 __revision__ = '$Format:%H$'
