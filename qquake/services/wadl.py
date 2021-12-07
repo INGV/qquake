13d061751720ad4cbffc44db3f28c5b05a9576a6
Nyall Dawson <nyall.dawson@gmail.com>
2021-12-07 12:54:02 +1000

    Start on wadl parser

---
+++ qquake/services/wadl.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+WADL to service configuration parser
+"""
+
+# .. note:: This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+__author__ = 'Original authors: Mario Locati, Roberto Vallone, Matteo Ghetta, Nyall Dawson'
+__date__ = '29/01/2020'
+__copyright__ = 'Istituto Nazionale di Geofisica e Vulcanologia (INGV)'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+from typing import Dict
+
+from .service_manager import ServiceManager
+
+from qgis.PyQt.QtCore import QByteArray
+from qgis.PyQt.QtXml import QDomDocument
+
+
+class WadlServiceParser:
+    """
+    WADL to service configuration parser
+    """
+
+    @staticmethod
+    def parse_wadl(content: QByteArray, service_type: str) -> Dict:
+        """
+        Parses a WADL document and converts to a service configuration
+        """
+        doc = QDomDocument()
+        doc.setContent(content)
+        resources_elements = doc.elementsByTagName('resources')
+        assert resources_elements.length() == 1
+
+        resources_element = resources_elements.at(0).toElement()
+
+        query_element = None
+        request_element = None
+        resource_elements = resources_element.elementsByTagName('resource')
+        for e in range(resource_elements.length()):
+            resource_element = resource_elements.at(e).toElement()
+
+            method_element = resource_element.firstChildElement('method').toElement()
+            if method_element.isNull():
+                continue
+
+            request_element = method_element.firstChildElement('request').toElement()
+            if request_element.isNull():
+                continue
+
+            query_element = resource_element
+            break
+
+        assert query_element
+        assert not request_element.isNull()
+
+        param_elements = request_element.elementsByTagName('param')
+        min_latitude = None
+        max_latitude = None
+        min_longitude = None
+        max_longitude = None
+        has_event_id = False
+        has_query_depth = False
+        include_all_origins = False
+        include_all_magnitudes = False
+        for e in range(param_elements.length()):
+            param_element = param_elements.at(e).toElement()
+
+            param_name = param_element.attribute('name')
+
+            if service_type != ServiceManager.FDSNSTATION:
+                if param_name == 'eventid':
+                    has_event_id = True
+                elif param_name in ('mindepth', 'maxdepth'):
+                    has_query_depth = True
+                elif param_name == 'includeallorigins':
+                    include_all_origins = True
+                elif param_name == 'includeallmagnitudes':
+                    include_all_magnitudes = True
+
+            if param_name == 'minlatitude':
+                min_latitude = float(param_element.attribute('default', '-180'))
+            elif param_name == 'maxlatitude':
+                max_latitude = float(param_element.attribute('default', '180'))
+            elif param_name == 'minlongitude':
+                min_longitude = float(param_element.attribute('default', '-90'))
+            elif param_name == 'maxlongitude':
+                max_longitude = float(param_element.attribute('default', '90'))
+
+        endpoint = resources_element.attribute('base')
+        if not endpoint.endswith('/'):
+            endpoint += '/'
+        resource_path = resource_element.attribute('path') + "?"
+        if resource_path.startswith('/'):
+            resource_path = resource_path[1:]
+        endpoint += resource_path
+
+        settings = {}
+        if service_type != ServiceManager.FDSNSTATION:
+            settings = {
+                "queryeventid": has_event_id,
+                "querydepth": has_query_depth,
+                "queryincludeallorigins": include_all_origins,
+                "queryincludeallmagnitudes": include_all_magnitudes
+            }
+
+        return {
+            "endpointurl": endpoint,
+            "boundingbox": [
+                min_longitude if min_longitude is not None else -180,
+                min_latitude if min_latitude is not None else -90,
+                max_longitude if max_longitude is not None else 180,
+                max_latitude if max_latitude is not None else 90
+            ],
+            "settings": settings
+        }
---
+++ qquake/test/test_wadl.py
@@ -0,0 +1,121 @@
+# coding=utf-8
+"""QuakeML parser test
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+import os
+import unittest
+
+from qgis.PyQt.QtCore import (
+    QByteArray
+)
+
+from qquake.services import WadlServiceParser, ServiceManager
+
+
+class TestWadl(unittest.TestCase):
+    """
+    Test WADL parsing
+    """
+
+    maxDiff = None
+
+    def test_event_wadl(self):
+        """
+        Test WADL event parsing
+        """
+        path = os.path.join(os.path.dirname(
+            __file__), 'data', 'event.wadl')
+        with open(path, 'rb') as f:
+            content = f.read()
+
+        byte_array = QByteArray(content)
+
+        res = WadlServiceParser.parse_wadl(byte_array, ServiceManager.FDSNEVENT)
+        self.assertEqual(res, {'boundingbox': [-120.0, -70.0, 60.0, 50.0],
+                               'endpointurl': 'http://webservices.rm.ingv.it/fdsnws/event/1/query?',
+                               'settings': {'querydepth': True,
+                                            'queryeventid': True,
+                                            'queryincludeallmagnitudes': True,
+                                            'queryincludeallorigins': True}})
+
+    def test_event_wadl2(self):
+        """
+        Test WADL event parsing 2
+        """
+        path = os.path.join(os.path.dirname(
+            __file__), 'data', 'event2.wadl')
+        with open(path, 'rb') as f:
+            content = f.read()
+
+        byte_array = QByteArray(content)
+
+        res = WadlServiceParser.parse_wadl(byte_array, ServiceManager.FDSNEVENT)
+        self.assertEqual(res, {'boundingbox': [5.0, 35.0, 21.0, 48.0],
+                               'endpointurl': 'https://www.emidius.mi.ingv.it/fdsnws/event/1/query?',
+                               'settings': {'querydepth': False,
+                                            'queryeventid': False,
+                                            'queryincludeallmagnitudes': False,
+                                            'queryincludeallorigins': False}})
+
+    def test_macro_1(self):
+        """
+        Test WADL macro parsing 1
+        """
+        path = os.path.join(os.path.dirname(
+            __file__), 'data', 'macro.wadl')
+        with open(path, 'rb') as f:
+            content = f.read()
+
+        byte_array = QByteArray(content)
+
+        res = WadlServiceParser.parse_wadl(byte_array, ServiceManager.MACROSEISMIC)
+        self.assertEqual(res, {'boundingbox': [-32.0, 33.0, 45.0, 74.0],
+                               'endpointurl': 'https://www.emidius.eu/services/macroseismic/query?',
+                               'settings': {'querydepth': False,
+                                            'queryeventid': True,
+                                            'queryincludeallmagnitudes': True,
+                                            'queryincludeallorigins': True}})
+
+    def test_macro_2(self):
+        """
+        Test WADL macro parsing 2
+        """
+        path = os.path.join(os.path.dirname(
+            __file__), 'data', 'macro2.wadl')
+        with open(path, 'rb') as f:
+            content = f.read()
+
+        byte_array = QByteArray(content)
+
+        res = WadlServiceParser.parse_wadl(byte_array, ServiceManager.MACROSEISMIC)
+        self.assertEqual(res, {'boundingbox': [5.0, 35.0, 21.0, 48.0],
+                               'endpointurl': 'https://www.emidius.mi.ingv.it/services/macroseismic/query?',
+                               'settings': {'querydepth': False,
+                                            'queryeventid': True,
+                                            'queryincludeallmagnitudes': True,
+                                            'queryincludeallorigins': True}})
+
+    def test_station(self):
+        """
+        Test station parsing
+        """
+        path = os.path.join(os.path.dirname(
+            __file__), 'data', 'station.wadl')
+        with open(path, 'rb') as f:
+            content = f.read()
+
+        byte_array = QByteArray(content)
+
+        res = WadlServiceParser.parse_wadl(byte_array, ServiceManager.FDSNSTATION)
+        self.assertEqual(res, {'boundingbox': [-90.0, -180.0, 90.0, 180.0],
+                               'endpointurl': '/fdsnws/station/1/query?',
+                               'settings': {}})
+
+
+if __name__ == '__main__':
+    unittest.main()
