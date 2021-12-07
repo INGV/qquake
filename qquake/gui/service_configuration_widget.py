d8c7e0b49c3650fd344044ebb7d1bceefa2ea83a
Nyall Dawson <nyall.dawson@gmail.com>
2021-12-07 13:28:31 +1000

    Fix dialog

--- qquake/gui/service_configuration_widget.py
+++ qquake/gui/service_configuration_widget.py
@@ -360,7 +360,7 @@ class ServiceConfigurationWidget(QWidget, FORM_CLASS):
             self.button_load_service.setText(self.tr('Load Web Service Capabilities'))
 
             content = _reply.readAll()
-            self._set_state_from_wadl(WadlServiceParser.parse_wadl(content, self.service_type))
+            self._set_state_from_wadl(WadlServiceParser.parse_wadl(content, self.service_type, url))
 
         self.button_load_service.setEnabled(False)
         self.button_load_service.setText(self.tr('Loading'))
