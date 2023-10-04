8efd12d29b75f4701f0c72801e89389e4da2e8de
Mario Locati <mario.locati@ingv.it>
2023-10-04 17:25:22 +0200

    Update parser.py

--- qquake/quakeml/parser.py
+++ qquake/quakeml/parser.py
@@ -384,13 +384,17 @@ class QuakeMlParser:
                         continue
 
                     event = [e for e in self.events if e.publicID == m.eventReference][0]
-                    source_obj = self.magnitudes[event.preferredMagnitudeID]
-                    for s in source:
-                        if source_obj is None:
-                            source_obj = NULL
-                            break
-                        assert hasattr(source_obj, s)
-                        source_obj = getattr(source_obj, s)
+                    
+                    try:
+                        source_obj = self.magnitudes[event.preferredMagnitudeID]
+                        for s in source:
+                            if source_obj is None:
+                                source_obj = NULL
+                                break
+                            assert hasattr(source_obj, s)
+                            source_obj = getattr(source_obj, s)
+                    except:
+                        source_obj = NULL
 
                     f[dest_field[field_config_key]] = source_obj
 
