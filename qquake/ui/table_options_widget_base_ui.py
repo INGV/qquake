4db551418830ab62c21e35588daacd240301b64c
Mario Locati <mario.locati@ingv.it>
2026-05-22 15:19:40 +0200

    Update import statement for PyQt5 module

--- qquake/ui/table_options_widget_base_ui.py
+++ qquake/ui/table_options_widget_base_ui.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\Users\giorg\AppData\Roaming\QGIS\QGIS3\profiles\qquake\python\plugins\qquake\ui\table_options_widget_base.ui'
+# Form implementation generated from reading ui file 'table_options_widget_base.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
 
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from qgis.PyQt import QtCore, QtGui, QtWidgets
 
 
 class Ui_table_options_widget_base(object):
